44a37fe05bb9102163633ad8f0ada3de1bb9410f
Venkataprasad-Gadi <pvenkat193@gmail.com>
2024-07-30 18:38:12 +0530

    Spring Cloud Config Server Changes

---
+++ spring-boot-cloundconfigserver1/src/main/java/com/venkat/springboot/cloudconfigserver1/SpringBootCloundConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.venkat.springboot.cloudconfigserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * SpringBootCloundConfigServerApplication.
+ *
+ * @author venkataprasad.g
+ * @version $Id: SpringBootCloundConfigServerApplication.java 24-Jul-2024 6:03:25 pm
+ *
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class SpringBootCloundConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloundConfigServerApplication.class, args);
+	}
+
+}
