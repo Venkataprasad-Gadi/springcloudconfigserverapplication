6ea5789036e8ba65f47f81c55fe22c065b6bb32b
Venkataprasad-Gadi <pvenkat193@gmail.com>
2024-08-03 23:58:50 +0530

    Created new sample service

---
+++ spring-boot-cloundconfigserver1/src/main/java/com/venkat/springboot/service/Service.java
@@ -0,0 +1,13 @@
+package com.venkat.springboot.service;
+
+/**
+ * Service.
+ *
+ * @author venkataprasad.g
+ * @version $Id: Service.java 03-Aug-2024 11:57:57 pm
+ *
+ * @since 1.0
+ */
+public class Service {
+
+}
